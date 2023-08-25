26c9bb0fc8b49a548a5cd15f746952e6debd5e74
Karthick <125793976+karthi200125@users.noreply.github.com>
2023-08-25 15:37:07 +0530

    update create post

--- routes/product.js
+++ routes/product.js
@@ -11,6 +11,17 @@ router.get("/find/:id", async (req, res) => {
   }
 });
 
+//CREATE A POST
+router.post("/", async (req, res) => {
+  const newProduct = new Product(req.body); 
+  try {
+    const savedProduct = await newProduct.save(); 
+    res.status(200).json(savedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 //GET ALL PRODUCTS
 router.get("/", async (req, res) => {
   const qNew = req.query.new;
